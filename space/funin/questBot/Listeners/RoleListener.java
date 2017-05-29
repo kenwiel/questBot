1fb533c5c9ad2d173f694056e227871109c9b002
kenwiel <kenwiel@hotmail.de>
2017-05-29 23:22:04 +0200

    Fix !!leave command

--- space/funin/questBot/Listeners/RoleListener.java
+++ space/funin/questBot/Listeners/RoleListener.java
@@ -1,19 +1,19 @@
 package space.funin.questBot.Listeners;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.entities.Channel;
 import de.btobastian.javacord.entities.Server;
 import de.btobastian.javacord.entities.User;
+import de.btobastian.javacord.entities.message.Message;
 import de.btobastian.javacord.entities.permissions.Role;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import space.funin.questBot.CommandResponses;
 import space.funin.questBot.QuestBot;
 import space.funin.questBot.Settings;
+import space.funin.questBot.utils.CommandUtils;
 
 /**
  * @author Ken Wieland
@@ -50,64 +50,27 @@ public class RoleListener implements CommandExecutor {
     
     @Command(aliases = { "!!leave", "!!l" }, description = "Leave the mentioned role(s)", usage = "!!leave [@mention]*",
             async = true)
-    public String onLeaveCommand(String[] args, User user, Channel channel, Server server) {
-        String roleID;
-        for (String s : args) {
-            if (s.startsWith("<@&") && s.endsWith(">")) {
-                // if the mentioned role is a quest
-                roleID = s.substring(3, s.length() - 1);
-                Role role = QuestBot.getRole(roleID, server);
-                
-                if (Settings.getMap().containsKey(roleID) && user.getRoles(server).contains(role)) {
-                    role.removeUser(user);
-                    channel.sendMessage("!!leave: Removed role " + role.getMentionTag());
-                } else {
-                    channel.sendMessage("!!leave: " + role.getMentionTag() + " : " + CommandResponses.errorInvalidRole);
-                }
+    public void onLeaveCommand(Message message, User user, Channel channel, Server server) throws InterruptedException, ExecutionException {
+        List<Role> mentions = message.getMentionedRoles();
+        
+        for(Role r : mentions) {
+        	if(Settings.getMap().containsKey(r.getId()) && user.getRoles(server).contains(r)) {
+        		r.removeUser(user);
+                channel.sendMessage("!!leave: Removed role " + r.getMentionTag());
+            } else {
+                channel.sendMessage("!!leave: " + r.getMentionTag() + " : " + CommandResponses.errorInvalidRole);
             }
         }
-        return "";
     }
     
-    @Command(aliases = { "!!addRank", "!!createRole", "!!create", "!!addRole", }, description = "Creates a quest Role",
-            usage = "!!addRank [roleName]", async = true)
-    public String onCreateCommand(String[] args, User user, Channel channel, Server server, DiscordAPI api) {
-        // command only available to moderators
-        List<Role> userRoles = new ArrayList<Role>(user.getRoles(server));
-        Role moderator = QuestBot.getRole("242403619108814850", server);
-        if (!userRoles.contains(moderator)) {
-            return "!!createRole: " + CommandResponses.errorPermissions;
-        }
-        
-        Role newRole = null;
-        try {
-            newRole = server.createRole().get();
-        } catch (InterruptedException | ExecutionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        if (newRole == null)
-            return "!!createRole: " + CommandResponses.errorNull;
-                
-        try {
-            newRole.updateName(args[0]).get();
-            return "Created role " + newRole.getMentionTag();
-        } catch (InterruptedException | ExecutionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return "!!createRole: " + CommandResponses.errorTimeout;
-    }
-    
-    @Command(aliases = { "!!removeRole", "!!deleteRole", "!!delRole" }, description = "Removes role(s)",
+    @Command(aliases = { "!!removeRole", "!!deleteRole", "!!delRole" }, description = "Removes role(s). Moderator Only.",
             usage = "!!removeRole [@role]*", async = true)
-    public String onRemoveCommand(String[] args, User user, Server server) {
+    public void onRemoveCommand(String[] args, User user, Channel channel, Server server) {
         // command only available to moderators
-        List<Role> userRoles = new ArrayList<Role>(user.getRoles(server));
-        Role moderator = QuestBot.getRole("242403619108814850", server);
-        if (!userRoles.contains(moderator)) {
-            return "!!removeRole: " + CommandResponses.errorPermissions;
-        }
+		if (!CommandUtils.isMod(user, server)) {
+			channel.sendMessage("!!mute : " + CommandResponses.errorPermissions);
+			return;
+		}
         
         String questID;
         for (String s : args) {
@@ -120,7 +83,8 @@ public class RoleListener implements CommandExecutor {
                     
                     try {
                         role.delete().get();
-                        return "!!removeRole: " + "removed Role";
+                        channel.sendMessage("!!removeRole: " + "removed Role");
+                        return;
                     } catch (InterruptedException | ExecutionException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
@@ -128,6 +92,6 @@ public class RoleListener implements CommandExecutor {
                 }
             }
         }
-        return "!!removeRole" + CommandResponses.errorArgMention;
+        channel.sendMessage("!!removeRole" + CommandResponses.errorArgMention);
     }
 }
