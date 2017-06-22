0eb9816c5444ed9a68218a7539bb2b88aa55700d
kenwiel <kenwiel@hotmail.de>
2017-06-22 18:23:14 +0200

    Added author ID to botInfo command
    didnt fire due to missing id before

--- space/funin/questBot/Listeners/Bot/BotInfoCommand.java
+++ space/funin/questBot/Listeners/Bot/BotInfoCommand.java
@@ -3,21 +3,25 @@ package space.funin.questBot.Listeners.Bot;
 import java.util.concurrent.ExecutionException;
 
 import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.entities.Channel;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 
 public class BotInfoCommand implements CommandExecutor {
 	DiscordAPI api;
+	String authorID = "156879547214725120";
+	
 	public BotInfoCommand(DiscordAPI api) {
 		this.api = api;
 	}
 	@Command(aliases = { "!!botinfo",
 			"!!bi" }, description = "Shows some information about the bot.", usage = "!!botinfo")
-	public String onInfoCommand(String[] args) throws InterruptedException, ExecutionException {
-		return "**INFO**\n- **Author:** " + api.getUserById("id").get().getMentionTag() + "\n"
+	public void onInfoCommand(String[] args, Channel channel) throws InterruptedException, ExecutionException {
+		String botInfo =  "**INFO**\n- **Author:** " + api.getUserById(authorID).get().getMentionTag() + "\n"
 				+ "- **Language:** Java\n"
 				+ "- **Library:** Javacord\n"
 				+ "- **Command-Lib:** sdcf4j\n"
 				+ "- **Github:** https://github.com/kenwiel/questBot/";
+		channel.sendMessage(botInfo);
 	}
 }
