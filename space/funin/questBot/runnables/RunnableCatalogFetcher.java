30b8f51c65c2455a5b13735adab1dfc20698f24e
kenwiel <kenwiel@hotmail.de>
2017-08-30 12:56:09 +0200

    Remove debug stuff from working build

--- space/funin/questBot/runnables/RunnableCatalogFetcher.java
+++ space/funin/questBot/runnables/RunnableCatalogFetcher.java
@@ -13,14 +13,15 @@ public class RunnableCatalogFetcher implements Runnable{
 			System.out.println("refreshing qst");
 			QuestBot.qst.refreshCache();
 			
-			List<Thread> threadList = new ArrayList<Thread>( QuestBot.qst.cache.values() );
+			//debug stuff, uncomment to print thread titles in console when refreshing
+			/*List<Thread> threadList = new ArrayList<Thread>( QuestBot.qst.cache.values() );
 	
 			
 			//iterate through all threads
 			for(Thread t : threadList) {
 				String subject = t.OriginalPost.subject();
 				System.out.println(subject);
-			} 
+			} */
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
