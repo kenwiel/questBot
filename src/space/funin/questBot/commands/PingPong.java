78425b514bbd071df63832860d0eb2583965e114
ken <kenwiel@hotmail.de>
2017-11-16 10:43:04 +0100

    changed ping command

--- src/space/funin/questBot/commands/PingPong.java
+++ src/space/funin/questBot/commands/PingPong.java
@@ -1,13 +1,15 @@
 package space.funin.questBot.commands;
 
-import de.btobastian.javacord.events.message.MessageCreateEvent;
+import de.btobastian.javacord.entities.message.Message;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 
 public class PingPong implements CommandExecutor {
 
-    @Command(aliases = { "ping10" })
-    public void onCall(MessageCreateEvent event) {
-        System.out.println("in");
+    @Command(aliases = { "ping" })
+    public void onCall(Message message) {
+        message.getServerTextChannel().ifPresent(
+                serverTextChannel -> serverTextChannel.sendMessage(
+                        message.getContent().replaceFirst("..ping ", "")));
     }
 }
